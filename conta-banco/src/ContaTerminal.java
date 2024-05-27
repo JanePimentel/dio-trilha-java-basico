eb163d93eb732d36c6b86faafcf4e6b8e5875218
JanePimentel <jane-lopes.10@hotmail.com>
2024-05-26 23:01:51 -0300

    Classe conta terminal com scanner para obter agencia,numero,saldo,nome,sobrenome e retornar usuario

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,31 @@
+
+import java.util.Scanner;
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.println("Por favor, digite seu nome!");
+        String nome = scanner.next();
+       
+        System.out.println("Por favor, digite seu sobrenome!");
+        String sobrenome = scanner.next();
+
+        System.out.println("Por favor, digite o número da conta!");
+        int numero = scanner.nextInt();
+        
+        System.out.println("Por favor, digite o número da Agência!");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, digite o saldo!");
+        double saldo = scanner.nextDouble();
+        while (saldo < 0){
+            System.out.println("Não é permitido inserir saldo negativo! \nPor favor, Digite novo valor");
+            saldo = scanner.nextDouble();
+        }
+
+        System.out.println("Olá, " + nome + " " + sobrenome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo de " + NumberFormat.getCurrencyInstance().format(saldo) + " já está disponível para saque.");
     }
 }
